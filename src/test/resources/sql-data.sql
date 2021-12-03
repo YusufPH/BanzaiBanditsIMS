62343dd7db3f7c840d43ab458c8140868ba2c850
Jamiejoule998 <95473044+jamiejoule998@users.noreply.github.com>
2021-12-03 14:16:57 +0000

    updating sql stuff, made it work with root,root. database now actually works

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,8 +1,14 @@
-INSERT INTO CUSTOMER (first_name, surname) VALUES ( "Bruno", "Zorima");
-INSERT INTO CUSTOMER (first_name, surname) VALUES ( "Saqlain", "Chowdhury");
-INSERT INTO CUSTOMER (first_name, surname) VALUES ( "Yusuf", "Pholby");
-INSERT INTO CUSTOMER (first_name, surname) VALUES ( "Jamie", "Joule");
-INSERT INTO CUSTOMER (first_name, surname) VALUES ( "Raymond", "Wang");
+INSERT INTO CUSTOMERS (first_name, surname) VALUES ( "Bruno", "Zorima");
+INSERT INTO CUSTOMERS (first_name, surname) VALUES ( "Saqlain", "Chowdhury");
+INSERT INTO CUSTOMERS (first_name, surname) VALUES ( "Yusuf", "Pholby");
+INSERT INTO CUSTOMERS (first_name, surname) VALUES ( "Jamie", "Joule");
+INSERT INTO CUSTOMERS (first_name, surname) VALUES ( "Raymond", "Wang");
+
+INSERT INTO ITEMS (ItemName, StockAmount, PRICE) VALUES ("willtolive", 1, 1.0);
+INSERT INTO ITEMS (ItemName, StockAmount, PRICE) VALUES ("Christmas tree", 2, 27.99);
+INSERT INTO ITEMS (ItemName, StockAmount, PRICE) VALUES ("Pixel Pro 6", 100, 899.99);
+INSERT INTO ITEMS (ItemName, StockAmount, PRICE) VALUES ("IPHONE 13 pro", 100, 1100);
+INSERT INTO ITEMS (ItemName, StockAmount, PRICE) VALUES ("PS5", 0, 899.99);
 
 INSERT INTO ORDERS (CustomerID) VALUES (1);
 INSERT INTO ORDERS (CustomerID) VALUES (1);
@@ -10,12 +16,6 @@ INSERT INTO ORDERS (CustomerID) VALUES (3);
 INSERT INTO ORDERS (CustomerID) VALUES (2);
 INSERT INTO ORDERS (CustomerID) VALUES (4);
 INSERT INTO ORDERS (CustomerID) VALUES (5);
-
-INSERT INTO ITEM (ItemName, StockAmount, PRICE) VALUES ("willtolive", 1, 1.0);
-INSERT INTO ITEM (ItemName, StockAmount, PRICE) VALUES ("Christmas tree", 2, 27.99);
-INSERT INTO ITEM (ItemName, StockAmount, PRICE) VALUES ("Pixel Pro 6", 100, 899.99);
-INSERT INTO ITEM (ItemName, StockAmount, PRICE) VALUES ("IPHONE 13 pro", 100, 1100);
-INSERT INTO ITEM (ItemName, StockAmount, PRICE) VALUES ("PS5", 0, 899.99);
 
 INSERT INTO ITEMORDERS (OrderID, ItemID) VALUES (1, 1);
 INSERT INTO ITEMORDERS (OrderID, ItemID,Amount) VALUES (2, 2, 2);
